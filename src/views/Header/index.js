c2297b632d1fd4da00b621e612b0e4c9fa13eeb4
Santy Dev <devsanty@icloud.com>
2022-12-05 11:43:58 -0500

    Feat: Configure Redux

---
+++ src/views/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+        <nav>
+            <p>Popular Movies</p>
+            <p>Top Rated</p>
+            <p>Upcoming Movies</p>
+        </nav>
+    </div>
+  );
+}
+
+export default Header;
